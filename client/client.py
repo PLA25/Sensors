6b96b0fa27a45aad8fb6c9bda11c5021dbd8f356
Martijn Vegter <mail@mvegter.me>
2018-02-25 17:40:27 +0100

    Object -> Array

--- client/client.py
+++ client/client.py
@@ -30,7 +30,7 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
-    data = {
+    data = [
         {
             'type': 'temperature',
             'value': temperature
@@ -39,7 +39,7 @@ while True:
             'type': 'humidity',
             'value': humidity
         }
-    }
+    ]
 
     try:
         to_unicode = unicode
