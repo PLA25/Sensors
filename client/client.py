1d931a24cf2cce35386ded9630e972d7509852f3
Martijn Vegter <mail@mvegter.me>
2018-03-05 13:53:26 +0100

    Added Gas and Light

--- client/client.py
+++ client/client.py
@@ -18,34 +18,85 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+print("Press CTRL+C to abort.")
+print("")
+
+# System
+import io
+import json
 import sys
-import Adafruit_DHT
+import time
 
-import json, io
-
+# System functions
 from os import remove
 from shutil import move
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+# Adafruit
+import Adafruit_DHT
 
+# MCP3008
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+# Gas (MQ-2) Sensor
+from mq import *
+mq = MQ();
+
+while True:
+    # LDR
+    luxVoltage = mcp.read_adc(0)
+    print("Light    : {}".format(luxVoltage))
+    
+    # Gas sensor
+    perc = mq.MQPercentage()
+    print("LPG      : {} ppm ({})".format(round(perc["GAS_LPG"], 10), perc["GAS_LPG"]))
+    print("CO       : {} ppm ({})".format(round(perc["CO"], 10), perc["CO"]))
+    print("Smoke    : {} ppm ({})".format(round(perc["SMOKE"], 10), perc["SMOKE"]))
+    
+    # Temp/Hum Sensor
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    print("Tempe    : {} C".format(temperature))
+    print("Humidity : {} %".format(humidity))
+    
+    # Sensordata
     data = [
         {
-            'type': 'temperature',
-            'value': temperature
+            'type': 'lux',
+            'value': luxVoltage
+        },
+        {
+            'type': 'lpg',
+            'value': perc["GAS_LPG"]
+        },
+        {
+            'type': 'co',
+            'value': perc["CO"]
+        },
+        {
+            'type': 'smoke',
+            'value': perc["SMOKE"]
         },
         {
             'type': 'humidity',
             'value': humidity
+        },
+        {
+            'type': 'temperature',
+            'value': temperature
         }
     ]
-
+    
+    # Attempt to use UniCode
     try:
         to_unicode = unicode
     except NameError:
         to_unicode = str
-
+        
+    # Convert to JSON
     with io.open('data.txt', 'w', encoding='utf8') as outfile:
         str_ = json.dumps(data,
                           indent=4,
@@ -55,5 +106,10 @@ while True:
         )
         outfile.write(to_unicode(str_))
 
+    # Trigger Node.JS watchFile
     remove('data.json')
     move('data.txt', 'data.json')
+    
+    # Beautify
+    print("")
+    time.sleep(5)
